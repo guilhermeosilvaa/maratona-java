2fffa108f6ca42377f8808e542a9237ab6b319dd
Guilherme Oliveira <guilhermeosilva2406@gmail.com>
2026-01-27 12:36:39 -0300

    27/01/2026

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,15 +4,15 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
         String name = "Guilherme";
-        int age = 28;
+        int age = (int) 1000000000000L; //cast
         long numberBig = 1000000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = 2500.0F;
         byte ageByte = 10;
-        short ageShort = 10;
+        short ageShort = 32000;
         boolean verdadeiro = true;
         boolean falso = false;
-        char caractere = '\u0041';
+        char caractere = 'M';
 
         System.out.println("A idade é de "+name+" é "+age+" anos" );
         System.out.println(falso);
