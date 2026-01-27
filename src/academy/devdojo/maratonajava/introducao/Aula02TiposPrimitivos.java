2c9d9ce8514e1835f6744485ce962c9895a31fa6
Guilherme Oliveira <guilhermeosilva2406@gmail.com>
2026-01-27 11:11:57 -0300

    new version

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -1,4 +1,21 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        String name = "Guilherme";
+        int age = 28;
+        long numberBig = 1000000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte ageByte = 10;
+        short ageShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+
+        System.out.println("A idade é de "+name+" é "+age+" anos" );
+        System.out.println(falso);
+        System.out.println("char "+caractere);
+    }
 }
