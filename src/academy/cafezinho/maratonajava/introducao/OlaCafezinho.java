63556de706882f53dd6104e0ff328cba16645326
Guilherme Oliveira <guilhermeosilva2406@gmail.com>
2026-03-15 16:49:04 -0300

    New name in arquived2

---
+++ src/academy/cafezinho/maratonajava/introducao/OlaCafezinho.java
@@ -0,0 +1,8 @@
+package academy.cafezinho.maratonajava.introducao;
+
+public class OlaCafezinho {
+    public static void main(String[] args){
+        System.out.println("Ka-ME-HA-ME-HA");
+
+    }
+}
