2bf8bfa927ab0136056523aba4af78e923dd5865
Guilherme Oliveira <guilhermeosilva2406@gmail.com>
2026-03-15 22:01:06 -0300

    New name in arquived6

--- src/academy/cafezinho/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/cafezinho/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,7 +3,7 @@ package academy.cafezinho.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
     // int, double, float, char, byte, short, long, boolean
-        int idade = 10;
-        System.out.println(idade);
+        int idade = 28;
+        System.out.println("A minha idade é: " +idade);
     }
 }
