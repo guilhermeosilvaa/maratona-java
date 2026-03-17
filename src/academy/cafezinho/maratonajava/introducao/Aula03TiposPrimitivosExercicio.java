81d70bad3f4dbfdf66076e8e611877db4091017a
Guilherme Oliveira <guilhermeosilva2406@gmail.com>
2026-03-17 09:38:53 -0300

    2026/03/17

--- src/academy/cafezinho/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
+++ src/academy/cafezinho/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -1,4 +1,21 @@
 package academy.cafezinho.maratonajava.introducao;
 
+import java.time.LocalDate;
+
+/*
+Praticar
+Crie variáveis para os campos descritos abaico <> e imprima a seguinte mensagem:
+Eu <nome>, morando no endereço <endereço>, confirmo que recebi o salario de <salario>, na data <data>
+ */
 public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Guilherme O. Silva";
+        String endereço = "Av Anjos das Graças";
+        double salarioRecebido = 3035.12;
+        LocalDate dataRecebimento = LocalDate.of(2026, 3, 17);
+
+        String relatorioDados = "Eu " +nome+ ", morando no endereço " +endereço+ ", confirmo que recebi o salario de " +
+                +salarioRecebido+ ", na data " +dataRecebimento;
+        System.out.println(relatorioDados);
+    }
 }
