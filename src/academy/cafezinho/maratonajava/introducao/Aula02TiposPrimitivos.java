490cb14aa9c89137f7d3b96b3fe8f651d6f8b1b4
Guilherme Oliveira <guilhermeosilva2406@gmail.com>
2026-03-16 21:36:26 -0300

    V4

--- src/academy/cafezinho/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/cafezinho/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,6 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
     // int, double, float, char, byte, short, long, boolean
         int idade = 28;
-        System.out.println("A minha idade é : " +idade);
+        System.out.println("A idade é: "+idade);
     }
 }
