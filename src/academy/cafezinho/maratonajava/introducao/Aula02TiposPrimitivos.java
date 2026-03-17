d9d4eef856c7707754ec7aab6971a4c1a6af57af
Guilherme Oliveira <guilhermeosilva2406@gmail.com>
2026-03-17 09:05:38 -0300

    2026/03/17

--- src/academy/cafezinho/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/cafezinho/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,19 +3,22 @@ package academy.cafezinho.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
     // int, double, float, char, byte, short, long, boolean
-        int idade = 28;
-        long numeroGrande = 1000000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
+        int idade = (int) 10000000000L; //cast
+        long numeroGrande = 1000000L;
+        double salarioDouble = 2000.0;
+        float salarioFloat = (float) 2500.0D;
         byte idadeByte = -128;
         short idadeShort = 10;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = 65;
+        String nome = "Cafezinho";
 
         System.out.println("A minha idade é "+idade+" anos");
         System.out.println(falso);
         System.out.println(caractere);
+        System.out.println(salarioFloat);
+        System.out.println("Oi meu nome é "+nome);
 
 
 
