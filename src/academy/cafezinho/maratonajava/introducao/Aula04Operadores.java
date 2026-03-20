727c1e10c1464bb4f90d884a5350b31823afedb9
Guilherme Oliveira <guilhermeosilva2406@gmail.com>
2026-03-20 09:25:55 -0300

    2026/03/20

--- src/academy/cafezinho/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/cafezinho/maratonajava/introducao/Aula04Operadores.java
@@ -11,5 +11,17 @@ public class Aula04Operadores {
         // %
         int resto = 21 % 7;
         System.out.println(resto);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte "+isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte "+isDezIgualVinte);
+        System.out.println("isDezIgualDez "+isDezIgualDez);
+        System.out.println("isDezDiferenteDez "+isDezDiferenteDez);
     }
 }
