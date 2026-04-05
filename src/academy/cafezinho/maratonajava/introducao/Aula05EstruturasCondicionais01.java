133bb5bf3468e0c1b66e5a5902af847c01d80bc5
Guilherme Oliveira <guilhermeosilva2406@gmail.com>
2026-04-05 18:13:50 -0300

    2026/04/05

--- src/academy/cafezinho/maratonajava/introducao/Aula05EstruturasCondicionais01.java
+++ src/academy/cafezinho/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -1,16 +1,19 @@
 package academy.cafezinho.maratonajava.introducao;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
         int idade = 15;
         boolean isAutorizadoComprarBebida = idade >= 18;
         // !
-        if (isAutorizadoComprarBebida) {
+        if (isAutorizadoComprarBebida != false) {
             System.out.println("Autotizado a comprar bebida alcólica");
+        }else {
+            System.out.println("Não Autotizado a comprar bebida alcólica");
         }
         if (!isAutorizadoComprarBebida){
             System.out.println("Não Autotizado a comprar bebida alcólica");
         }
+
         System.out.println("Fora do if");
     }
 }
