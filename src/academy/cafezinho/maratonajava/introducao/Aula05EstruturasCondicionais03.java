a43e97dc90fdf5edc8819071537bf3971dc91fd5
Guilherme Oliveira <guilhermeosilva2406@gmail.com>
2026-04-06 22:43:00 -0300

    2026/04/06

--- src/academy/cafezinho/maratonajava/introducao/Aula05EstruturasCondicionais03.java
+++ src/academy/cafezinho/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -3,11 +3,11 @@ package academy.cafezinho.maratonajava.introducao;
 public class Aula05EstruturasCondicionais03 {
     public static void main(String[] args) {
         // Doar se salario > 5000
-        double salario = 6000;
+        double salario = 7000;
         String mensagemDoar = "Eu vou doar 500 para o DevDojo";
         String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter!";
         // (condicao) ? verdadeiro : falso
-        String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
+        String resultado = salario > 6000 ? mensagemDoar : mensagemNaoDoar;
         System.out.println(resultado);
     }
 }
